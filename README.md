4f0d0dc900729d81c24b4f4cfdd549e16b0ac15b
Bobbert Simpson <prototribe@protonmail.com>
2024-08-02 04:09:26 +0700

    started implementing the encoder-decoder architecture

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 This is my implementation of the original transformer paper ([Vaswani et al.](https://arxiv.org/abs/1706.03762)) in PyTorch.
 As of now it'll mostly include the implementation details of the origial paper. However, in the future I might improve it to include explainations for other newcommers
 
+## Techniques Used
+While reading/implmenenting the paper I came across a bunch of NLP concepts and algorithms that I wasn't familiar with before.
+#### Dropout
+Dropout is the zeroing out of random elements of the input vector with probability p during training. This is done to avoid overfitting.
+#### Label Smoothing
--- transformer.py
+++ transformer.py
@@ -1,11 +1,50 @@
+from copy import deepcopy
 import torch
 from torch import nn
 import math
 
 class Transformer(nn.Module):
-    def __init__(self, d_model: int = 512, n_heads: int = 8, ):
+    def __init__(self, d_model: int = 512, encoder: Encoder, decoder: Decoder, src_vocab_size: int, tgt_vocab_size: int):
         super().__init__()
-        self.d_model = d_model
-        self.n_heads = n_heads
-    def forward(self, )
+        self.encoder = encoder
+        self.decoder = decoder
+        self.d_model = d_model # The dimention of the model (the size of the embeddings)
+        self.src_embed = Embedding(src_vocab_size, d_model) # Get the embeddings for the source
+        self.tgt_embed = Embedding(tgt_vocab_size, d_model) # Get the embeddings for the target
+        self.generator = Generator(model_dimension, trg_vocab_size) # Applies nn.Linear to the output of the final decoder layer and returns the probabilities using the sigmoid function
+        self.init_params() # Initialize all of the weights of the model
+
+    def forward(self, src, tg):
+             
+    def init_params(self):
+        for param in self.parameters():
+            if param.dim() > 1: # nn.init.xavier_transform_ only works for dim > 1
+                nn.init.xavier_transform_(param)
+
+class Generator(nn.Module):
+    def __init__(self, d_model: int, trg_vocab_size: int):
+        super().__init__()
+        self.linear = nn.Linear(d_model, trg_vocab_size)
+        self.log_softmax = nn.LogSoftmax(dim=-1);
+    def forward(self):
         
+class Encoder(nn.Module):
+    def __init__(self, d_model: int, n: int):
+        self.layers = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def clones(module, n):
+    # This is used to create copies of the encoder and decoder layers
+    return nn.ModuleList([deepcopy(module) for _ in range(n)])
